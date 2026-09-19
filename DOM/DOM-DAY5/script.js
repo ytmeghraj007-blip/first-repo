85c23ed32f214da88c5a58b92854208ace2a6b17
ytmeghraj007-blip <ytmeghraj007@gmail.com>
2026-09-19 15:43:59 +0530

    game

--- DOM/DOM-DAY5/script.js
+++ DOM/DOM-DAY5/script.js
@@ -3,6 +3,9 @@ const box = document.querySelector(".box2");
 const btn = document.querySelector(".btn1");
 const timer = document.querySelector("#timer");
 const score = document.querySelector("#score")
+const parent = document.querySelector(".box1");
+const overlay = document.querySelector(".overlay");
+const final = document.querySelector("#final");
 
 const color = ()=>{
     let r = Math.floor(Math.random()*256);
@@ -11,24 +14,49 @@ const color = ()=>{
     return `rgb(${r},${g},${b})`;
     
 }
-
+let interval;
+let timeout ;
 let time = 0;
+let scoree = 0;
 const position = ()=>{
     box.style.backgroundColor = color();
-    const rH = Math.floor(Math.random()*100);
-   const rW = Math.floor(Math.random()*100);
+   
+    let mainH = parent.clientHeight - box.offsetHeight;
+  let mainW = parent.clientWidth - box.offsetWidth;  
 
-    box.style.top = `${rH}%`;
-    box.style.left = `${rW}%`;
+  const rY = Math.random() * mainH;
+  const rX = Math.random() * mainW;
+
+    box.style.top = `${rY}px`;
+    box.style.left = `${rX}px`;
 }
 btn.addEventListener('click',()=>{
    
-position();
+clearInterval(interval);
 
-setInterval(() => {
+
+interval = setInterval(() => {
     time += 1;
+    position();
     timer.textContent = time;
 },1000);
 
+timeout = setTimeout(() => {
+    clearInterval(interval);
+    overlay.style.display = "flex";
+    setInterval(() => {
+        overlay.style.display = "none";
+    }, 1000);
+    final.innerHTML = scoree;
+}, 5000);
+
 });
 
+box.addEventListener('click',()=>{
+
+    scoree += 1;
+    setInterval(() => {
+        scoree = scoree;
+    }, 1000);
+    score.textContent = scoree;
+})
